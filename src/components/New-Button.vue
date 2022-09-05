2311f3ef156bbb5c92a8e892d174dec92789a840
Spencer Kwon <spencerkwon92@gmail.com>
2022-09-05 03:47:14 -0600

    created button and module components

---
+++ src/components/New-Button.vue
@@ -0,0 +1,53 @@
+<template>
+  <div :style="cssVars">
+    <button @click="onClick" :style="cssVars">
+      {{ buttonName }}
+    </button>
+  </div>
+  <div v-if="isModule">
+    <Module header="This is header" @close="onClick">
+      <h3>Here is contents</h3>
+    </Module>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import Module from "./New-Module.vue";
+
+export default defineComponent({
+  data() {
+    return {
+      isModule: false,
+    };
+  },
+  methods: {
+    onClick() {
+      this.isModule = !this.isModule;
+    },
+  },
+  props: ["buttonName", "backgroundColor", "size", "fontColor", "center"],
+  computed: {
+    cssVars() {
+      return {
+        "--backgroundColor": this.backgroundColor,
+        "--fontColor": this.fontColor,
+        "--fontSize": this.size,
+        "--center": this.center,
+      };
+    },
+  },
+  components: { Module },
+});
+</script>
+
+<style lang="scss" scoped>
+div {
+  text-align: var(--center);
+}
+button {
+  background-color: var(--backgroundColor);
+  color: var(--fontColor);
+  font-size: var(--fontSize);
+}
+</style>
--- src/views/ExampleView.vue
+++ src/views/ExampleView.vue
@@ -4,16 +4,26 @@
     <div class="box-container">
       <ExampleCard v-for="n in 16" :key="n" />
     </div>
+    <Button
+      buttonName="Click Me!"
+      fontColor="white"
+      backgroundColor="black"
+      size="50px"
+      center="center"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+
 import ExampleCard from "@/components/example/ExampleCard.vue";
+import Button from "@/components/New-Button.vue";
 
 export default defineComponent({
   components: {
     ExampleCard,
+    Button,
   },
 });
 </script>
