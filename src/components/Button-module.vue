185be1a64ac852682b2b7a3cf29c4f215e00cbaf
Spencer Kwon <spencerkwon92@gmail.com>
2022-09-05 04:29:30 -0600

    Created button and module components.

---
+++ src/components/Button-module.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="backdrop" @click="closeModule">
+    <div class="modal">
+      <h1>{{ header }}</h1>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+  methods: {
+    closeModule() {
+      this.$emit("close");
+    },
+  },
+  props: ["header"],
+});
+</script>
+
+<style lang="scss" scoped>
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background-color: white;
+  border-radius: 10px;
+}
+.backdrop {
+  top: 0;
+  position: fixed;
+  background-color: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+</style>
